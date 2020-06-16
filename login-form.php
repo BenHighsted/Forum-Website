3054a1a8d4e0fb2791c30e0ee0f00cdcf4f2ad02
BenHighsted <benhigh18@gmail.com>
2020-06-17 10:59:27 +1200

    Added style for login/register forms

--- login-form.php
+++ login-form.php
@@ -7,18 +7,23 @@
 <html>
 
     <head>
-        <!-- link rel="stylesheet" href="style.css" -->    
+        <link rel="stylesheet" href="style.css">
         <title>Sign In</title>
     </head>
 
     <body>
+        <div class="detail-container">
 
-        <form action="login.php" method="POST">
-            Username: <input type="text" name="username" placeholder="Username"/> <br/>
-            Password: <input type="password" name="password" placeholder="Password"/> <br/>
-            <input type="submit" name="login" value="Sign In"/>
-        </form>
+            <form action="login.php" method="POST">
+                <label for="username">Username:</label>
+                <input type="text" name="username" placeholder="Username"/> <br/>
+                <label for="password">Password:</label>
+                <input type="password" name="password" placeholder="Password"/> <br/>
 
+                <input type="submit" name="login" value="Sign In"/>
+            </form>
+
+        </div>  
     </body>
 
 </html>
