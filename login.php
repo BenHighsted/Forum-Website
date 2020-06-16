aa88b933f66226822e2722437f060d76ce60a293
BenHighsted <benhigh18@gmail.com>
2020-06-16 13:18:47 +1200

    Login form now working (querying the database)

--- login.php
+++ login.php
@@ -1,6 +1,30 @@
 <?php
+    include("connect.php");
 
     $username = $_POST["username"];
     $password = $_POST["password"];
+
+    if ($username == "") {
+        echo "Username can not be blank";
+    } else if ($password == "") {
+        echo "Password can not be blank";
+    } else {
+        // query database to find the username they entered, and check if the passwords match
+        if ($result = $mysqli -> query("
+            SELECT * from forum.users
+            WHERE username = '$username';"
+            )) 
+        {
+            $row = $result -> fetch_assoc();
+
+            if(password_verify($password, $row['password'])){
+                echo "Good work! You're password is correct :)";
+            } else {
+                echo "Incorrect password.";
+            }
+        } else {
+            echo "Username not found in database";
+        }
+    }
 
 ?>
