76c093f11f2f308ba43d0a6e4e5b70208caa258c
BenHighsted <benhigh18@gmail.com>
2020-06-16 23:37:03 +1200

    Added logout button/methods

--- index.php
+++ index.php
@@ -13,12 +13,29 @@
 
     <body>
         <?php
-            if ($_SESSION["logged-in"]) {
+            if ($_SESSION['logged-in']) {
                 $username = $_SESSION['username'];
                 echo "<p> Welcome $username! </p>";
             } else {
         ?>
                 <p><a href="login-form.php">Login</a></p>
+        <?php
+            }
+        ?>
+
+        <?php 
+            if($_SESSION['logged-in']) {
+        ?>
+                <form action="index.php" method="POST">
+                    <input type="submit" value="Logout" name="logout"/>
+                </form>
+
+        <?php 
+            if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['logout'])){
+                $_SESSION['logged-in'] = false; 
+                header("Location: index.php"); 
+            }
+        ?>
         <?php
             }
         ?>
